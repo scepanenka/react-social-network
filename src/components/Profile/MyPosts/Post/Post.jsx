929ab27b5a64a701285e4b9c9a67bfa5b2fc2993
scepanenka <scepanenka@gmail.com>
2019-09-17 23:05:02 +0300

    feat: add new components

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Post.module.css'
+
+const Post = () => {
+  return (
+    <div className={s.post}>
+      post Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad corporis magni optio. Adipisci at deleniti dicta earum fugit maxime molestias pariatur quidem reprehenderit repudiandae, sint sunt? Deleniti dolorem quos voluptatibus.
+    </div>
+  )
+}
+
+export default Post;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <MyPosts/>
+        </div>
+    )
+};
+
+export default Profile;
