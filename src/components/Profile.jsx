ecc700b05ca43804e3dfe7172f10b44493b1656a
scepanenka <scepanenka@gmail.com>
2019-09-17 20:45:20 +0300

    feat: add css-modules

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Profile.module.css'
+
+const Profile = () => {
+  return (
+    <div className={s.content}>
+      profile
+    </div>
+  )
+}
+
+export default Profile;
