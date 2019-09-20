3fc56257e6540c2fc305951aae9a3b84bea7b596
scepanenka <scepanenka@gmail.com>
2019-09-20 23:41:19 +0300

    feat: add routes

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,9 +5,9 @@ import Post from "./Post/Post";
 const MyPosts = () => {
     return (
         <div>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post message='Lorem ipsum dolor sit amet, consectetur adipisicing elit. '/>
+            <Post message={'Animi asperiores cupiditate delectus eos expedita facilis\n' +
+            '    id, illum in ipsum laborum natus, nostrum sunt totam unde vel. Assumenda quae repudiandae ullam.'}/>
         </div>
     )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <MyPosts/>
         </div>
     )
