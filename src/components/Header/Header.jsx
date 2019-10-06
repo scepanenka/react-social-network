d2adbc08fe1bc85f6c17f1dbf3c5615d2a4b699d
scepanenka <scepanenka@gmail.com>
2019-10-06 12:32:59 +0300

    refactor: change logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className={s.header}>
       <div>
-        < img className={s.logo} src="https://i.dlpng.com/static/png/1235530-free-icons-png-vector-icon-smiley-smiley-png-1024_1024_preview.png" alt="logo" />
+        < img className={s.logo} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt="logo" />
       </div>
       <div className={s.title}>My social network</div>
     </header>
